51ac5a3833f9a49020e58a9b5cef4cb8be185c51
Khadiza Khatun <freelancerkhadiza2@gmail.com>
2024-11-19 21:48:36 +0600

    added edit files

--- app.js
+++ app.js
@@ -1,53 +1,56 @@
-<script id="script" type="text/Javascript" >
-
-   (function($){
-      $(document).ready(function(){
-          
-      $(".fast-menu a").click(function(){
-             
-     $(".second-menu ul li").slideToggle();
-             
-         })
-      })
-       
-   })(jQuery)    
-    
+(function ($) {
+    'use strict';
 
 
-   (function($){
-        $(document).ready(function(){
-          
-            $(".third-menu a").click(function(){
-                
+
+    $(document).ready(function () {
+
+        $(".third-menu a").click(function () {
+
             $(".fore-menu ul").slideToggle();
-             
+
         })
+
     })
-       
-   })(jQuery)    
+
+    $(document).ready(function () {
+
+        $(".fast-menu a").click(function () {
+
+            $(".second-menu ul li").slideToggle();
+
+        })
+
+    })
+
+
+    $('.to-top a').click(function () {
+        $('html, body').animate({ 'scrollTop': 0 }, 1000);
+
+        return false;
+
+    })
+
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 500){
+            $('.to-top a').fadeIn(1000);
+        }else{
+            $('.to-top a').fadeOut(1000);
+        };
     
+    }); 
+
+
+    // // Sticky Menu
+    // $(window).scroll(function () {
+    //     if ($('.navigation').offset().top > 100) {
+    //         $('.navigation').addClass('nav-bg');
+    //     } else {
+    //         $('.navigation').removeClass('nav-bg');
+    //     }
+    // });
 
 
 
-  (function($){ 
-      
-      $('.to-top a').click(function(){
-      $('html, body').animate({'scrollTop':0},1000);
-       
-      return false;  
-          
-      });
-        
-      $(window).scroll(function(){
-          if($(window).scrollTop() > 600){
-              $('.to-top a').fadeIn(1000);
-          }else{
-              $('.to-top a').fadeOut(1000);
-          };
-  
-       });
-      
-       
-   })(jQuery);  
-    
-</script>
+})(jQuery);
